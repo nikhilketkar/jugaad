18e1799e9b0b4831db3ffb84ca66a07df6ba63db
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-05 11:12:19 +0530

    ..

--- scripts/contextToCategory.py
+++ scripts/contextToCategory.py
@@ -1,4 +1,7 @@
 import sys
+from collections import defaultdict, Counter
+
+contextToCategory = defaultdict(list)
 
 with open(sys.argv[1]) as inputFile:
     recordsProcessed = 0
@@ -6,7 +9,13 @@ with open(sys.argv[1]) as inputFile:
         PID, MPID, URL, Title, Category, ImageUrl, ScoreFeatures = line.strip().split("\t")
         firstScoreFeature = ScoreFeatures.split(";;")[0]
         pageNumber, positionOnPage, seederPath, crawlTimeStamp = firstScoreFeature.split("||")
+        contextToCategory[seederPath].append(Category)
         recordsProcessed += 1
-    sys.stderr.write(str(recordsProcessed) + " records parsed successfully.\n" )
+    sys.stderr.write(str(recordsProcessed) + " records processed successfully.\n" )
 
-        
+with open(sys.argv[2], 'w') as outputFile:
+    for key, value in contextToCategory.items():
+        counter = Counter(value)
+        total = float(sum(counter.values()))
+        category, count = counter.most_common(1)[0]
+        outputFile.write(str(count/total) + "\t" + category + "\t" + key + "\n")
