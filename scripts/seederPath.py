4bbbb6c2ed712f61f12f09bfe9c156a965556b4b
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-05 19:52:42 +0530

    ..

---
+++ scripts/seederPath.py
@@ -0,0 +1,17 @@
+import sys
+
+seederPaths = set([])
+
+with open(sys.argv[1]) as inputFile:
+    recordsProcessed = 0
+    for line in inputFile:
+        PID, MPID, URL, Title, ImageUrl, Category, ScoreFeatures = line.strip().split("\t")
+        firstScoreFeature = ScoreFeatures.split(";;")[0]
+        pageNumber, positionOnPage, seederPath, crawlTimeStamp = firstScoreFeature.split("||")
+        seederPaths.append(seederPath)
+        recordsProcessed += 1
+    sys.stderr.write(str(recordsProcessed) + " records processed successfully.\n" )
+
+with open(sys.argv[2], 'w') as outputFile:
+    for seeederPath in seederPaths:
+        outputFile.write(seederPath + "\n")
