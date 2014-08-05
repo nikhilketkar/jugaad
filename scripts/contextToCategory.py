3405b5f413d13fcf9565949623b30ce28d8a0d84
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-05 10:42:40 +0530

    ..

--- scripts/contextToCategory.py
+++ scripts/contextToCategory.py
@@ -1,8 +1,12 @@
 import sys
 
 with open(sys.argv[1]) as inputFile:
+    recordsProcessed = 0
     for line in inputFile:
         PID, MPID, URL, Title, Category, ImageUrl, ScoreFeatures = line.strip().split("\t")
         firstScoreFeature = ScoreFeatures.split(";;")[0]
         pageNumber, positionOnPage, seederPath, crawlTimeStamp = firstScoreFeature.split("||")
+        recordsProcessed += 1
+    sys.stderr.write(str(recordsProcessed) + " records parsed successfully.\n" )
+
         
