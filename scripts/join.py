a6bb6fbbc4ad59b58e28a19289b7997595bc95c9
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-06 21:45:27 +0530

    ..

--- scripts/join.py
+++ scripts/join.py
@@ -4,8 +4,8 @@ url2category = {}
 with open(sys.argv[1]) as inputFile:
     for line in inputFile:
         words = line.split('\t')
-        url = words[0]
-        category = [4]
+        url = words[0].strip()
+        category = [4].strip()
         url2category[url] = category
 
 url2category = {}
@@ -13,5 +13,5 @@ with open(sys.argv[2]) as inputFile:
     with open(sys.argv[3], 'w') as outputFile:
         for line in inputFile:
             words = line.split('\t')
-            url = words[2]
+            url = words[2].strip()
             outputFile.write(line.strip() + "\t" + url2category[url] + "\n")
