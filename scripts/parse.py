93890da3522a9384630e704a4179bb1ea67d7b3f
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-07 11:41:56 +0530

    ..

---
+++ scripts/parse.py
@@ -0,0 +1,22 @@
+import sys
+
+import lxml.html
+
+with open(sys.argv[1]) as logFile:
+    with open(sys.argv[2]) as outputFile:
+        for line in logFile:
+            try:
+                words = line.strip().split('\t')
+                status = words[0]
+                url = words[1]
+                path = words[2]
+                with open(path) as htmlFile: htmlData = htmlFile.read()
+                root = lxml.html.fromstring(htmlData)
+                if status == "SUCCESS":
+                    try:
+                        text = " ".join([j.strip() for j in root.cssselect("#s-result-count")[0].itertext()])
+                    except:
+                        text = " ".join([j.strip() for j in root.cssselect("#noResultsTitle")[0].itertext()])
+                    outputFile.write(url + "\t" + text + "\n")                
+            except Exception as e:
+                sys.stderr.write("ERROR " + str(e) + "\t" + line)
