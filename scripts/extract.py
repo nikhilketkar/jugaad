244e8c0f429de6113f9c6b0f20c533f711254cc3
nikhil <nikhil.s.ketkar@gmail.com>
2014-08-19 20:08:20 +0530

    ..

--- scripts/extract.py
+++ scripts/extract.py
@@ -5,7 +5,7 @@ with open(sys.argv[1]) as inputFile:
     for line in inputFile:
         words = line.split('\t')
         if words[0] == "SUCCESS":
-            with open(words[2]) as currFile: 
+            with open(words[2].strip()) as currFile: 
                 data = currFile.read()
                 root = lxml.html.fromstring(data)
                 breadCrumb = "".join([i.strip() for i in root.cssselect('#s-result-count')[0].itertext()][1:]).replace(":", " > ")
