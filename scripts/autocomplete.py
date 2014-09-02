16d0b4a83e24042209db779340cee543b90a021f
nikhil <nikhil.s.ketkar@gmail.com>
2014-09-02 12:05:55 +0530

    ..

--- scripts/autocomplete.py
+++ scripts/autocomplete.py
@@ -12,8 +12,13 @@ prefixSet = set([])
 for prefix in genPrefix():
     prefixSet.add(prefix)
 
-with open(sys.argv[1],'w') as outputFile:
+with open(sys.argv[2],'w') as outputFile:
     for prefix in prefixSet:
-        url = "http://completion.amazon.com/search/complete?method=completion&q=" + urllib2.quote(prefix) + "&search-alias=aps&client=amazon-search-ui&mkt=1"
+        if sys.argv[1] == "amazon":
+            url = "http://completion.amazon.com/search/complete?method=completion&q=" + urllib2.quote(prefix) + "&search-alias=aps&client=amazon-search-ui&mkt=1"
+        elif sys.argv[1] == "shopping":
+            url = "http://www.shopping.com/ajaxSearchAssistant?q=" + urllib2.quote(prefix)
+        elif sys.argv[1] == "thefind":
+            url = "http://www.thefind.com/search/suggest.js?&q=" + urllib2.quote(prefix)
         outputFile.write(url + "\n")
         # outputFile.write(prefix + "\n")
