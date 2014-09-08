2916f782496473910dc811b409c52f48359bb953
nikhil <nikhil.s.ketkar@gmail.com>
2014-09-08 09:52:53 +0530

    ..

--- scripts/genGSqueries.py
+++ scripts/genGSqueries.py
@@ -5,5 +5,5 @@ def genQuery(query):
     return "https://www.google.com/search?&tbm=shop&q=" + urllib2.quote(query)
 
 for line in sys.stdin:
-    sys.stdout.write(genQuery(line.strip()))
+    sys.stdout.write(genQuery(line.strip()) + "\n")
 
