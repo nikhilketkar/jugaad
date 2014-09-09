f578dee879bd215991e9249213cd4117aadba9c2
nikhil <nikhil.s.ketkar@gmail.com>
2014-09-09 18:05:20 +0530

    ..

--- scripts/cleanGSearch.py
+++ scripts/cleanGSearch.py
@@ -8,12 +8,15 @@ def getURLs(root):
     return [i.getchildren()[0].attrib["href"] for i in root.cssselect('.r')]
 
 for line in sys.stdin:
-    filename = line.strip()
-    with open(filename) as inputFile:
-        data = inputFile.read()
-    root = lxml.html.fromstring(data)
-    searchString = getSearchString(root)
-    urls = getURLs(root)
-    for url in urls:
-        sys.stdout.write(searchString + "\t" + url + "\n")
+    try:
+        filename = line.strip()
+        with open(filename) as inputFile:
+            data = inputFile.read()
+        root = lxml.html.fromstring(data)
+        searchString = getSearchString(root)
+        urls = getURLs(root)
+        for url in urls:
+            sys.stdout.write("SUCCESS" + "\t" + filename  + "\t" + searchString + "\t" + url + "\n")
+    except:
+            sys.stdout.write("FALIURE" + "\t" + filename  + "\n")
         
