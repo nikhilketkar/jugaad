0ec01d1ece0b65153315ff6f2499b9e9a39bf265
nikhil <nikhil.s.ketkar@gmail.com>
2014-09-10 11:26:11 +0530

    ..

--- scripts/extractChildRefineMentLinks.py
+++ scripts/extractChildRefineMentLinks.py
@@ -6,15 +6,17 @@ for line in sys.stdin:
         STATUS, AMAZON_URL, FILENAME = line.strip().split('\t')
         with open(FILENAME) as inputFile:
             data = inputFile.read()
-        root = lxml.html.fromstring(data)
-        refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.childRefinementLink')]
-        if len(refinementURLs) < 1:
-            refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.boldRefinementLink')]
-        if len(refinementURLs) > 0:
-            for refinementURL in refinementURLs:
-                sys.stdout.write("SUCCESS" + "\t" + AMAZON_URL + "\t" + refinementURL + "\n")
-        else:
+        try:
+            root = lxml.html.fromstring(data)
+            refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.childRefinementLink')]
+            if len(refinementURLs) < 1:
+                refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.boldRefinementLink')]
+            if len(refinementURLs) > 0:
+                for refinementURL in refinementURLs:
+                    sys.stdout.write("SUCCESS" + "\t" + AMAZON_URL + "\t" + refinementURL + "\n")
+            else:
+                sys.stdout.write("FAILURE" + "\t" + AMAZON_URL + "\n")
+        except:
             sys.stdout.write("FAILURE" + "\t" + AMAZON_URL + "\n")
-
         
         
