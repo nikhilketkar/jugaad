c776c3a8517bbd1b8fe604da8abb94c1f5c248c7
nikhil <nikhil.s.ketkar@gmail.com>
2014-09-10 11:11:17 +0530

    ..

---
+++ scripts/extractChildRefineMentLinks.py
@@ -0,0 +1,20 @@
+import sys
+import lxml.html
+
+for line in sys.stdin:
+    if line.startswith("SUCCESS"):
+        STATUS, AMAZON_URL, FILENAME = line.strip().split('\t')
+        with open(FILENAME) as inputFile:
+            data = inputFile.read()
+        root = lxml.html.fromstring(data)
+        refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.childRefinementLink')]
+        if len(leafUrls) < 1:
+            refinementURLs = ["http://www.amazon.com" + i.getparent().attrib['href'] for i in root.cssselect('.boldRefinementLink')]
+        if len(leafUrls) > 0:
+            for refinementURL in refinementURLs:
+                sys.stdout.write("SUCCESS" + "\t" + AMAZON_URL + "\t" + refinementURL + "\n")
+        else:
+            sys.stdout.write("FAILURE" + "\t" + AMAZON_URL + "\n")
+
+        
+        
